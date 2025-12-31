d9e480d04278bba7fbab579e595df1a2bbdf392d
Shubhashis Roy <shubhashisroy360@gmail.com>
2025-12-31 15:38:44 +0530

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express from "express";
 import routes from "./routes";
+import cors from "cors";
 
 export const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // Middlewares
 app.use(express.json({ limit: "10kb" }));
